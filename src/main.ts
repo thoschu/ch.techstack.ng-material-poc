10682f9cffbeaa5698d6e87a708fece47c0b95ab
thoschu <thoschulte@gmail.com>
2023-05-25 11:40:36 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => console.log('Bootstrap success'))
   .catch(err => console.error(err));
