8a9a8af9c7e2f56c038bd38313478e2d0a75d9aa
Tom S <thoschulte@gmail.com>
2023-05-29 01:08:40 +0200

    update

--- src/app/step-one/step-one.component.ts
+++ src/app/step-one/step-one.component.ts
@@ -7,18 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./step-one.component.scss']
 })
 export class StepOneComponent {
-  private readonly telRegex: RegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
   public readonly formGroup: FormGroup;
 
   constructor(private readonly formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email, Validators.minLength(5)]],
-      category: ['NEW', Validators.required],
-      releasedAt: [new Date(), Validators.required],
-      downloadsAllowed: [true, Validators.requiredTrue],
-      description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      tel: ['', [Validators.required, Validators.pattern(this.telRegex)]],
-      option: ['', Validators.required]
+      releasedAt: [new Date(), Validators.required]
     });
   }
 }
