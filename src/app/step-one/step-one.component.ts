ec483f17344891239a6f328bca0089d80b3582eb
Tom S <thoschulte@gmail.com>
2023-05-29 13:38:02 +0200

    update

--- src/app/step-one/step-one.component.ts
+++ src/app/step-one/step-one.component.ts
@@ -4,22 +4,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-step-one',
   templateUrl: './step-one.component.html',
-  styleUrls: ['./step-one.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated
+  styleUrls: ['./step-one.component.scss']
 })
 export class StepOneComponent {
-  private static readonly LOREM_IPSUM_TEXT: string = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
-    vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent,
-    luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet.`;
+  private static readonly LOREM_IPSUM_TEXT: string = `Lorem ipsum dolor sit amet.`;
   public readonly formGroup: FormGroup;
 
   constructor(private readonly formBuilder: FormBuilder) {
