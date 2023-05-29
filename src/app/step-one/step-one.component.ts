a06653e1a1a13e8b2d37970fedcbdbb62ad9e171
Tom S <thoschulte@gmail.com>
2023-05-29 13:36:48 +0200

    update

--- src/app/step-one/step-one.component.ts
+++ src/app/step-one/step-one.component.ts
@@ -1,32 +1,30 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormControlState, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-step-one',
   templateUrl: './step-one.component.html',
   styleUrls: ['./step-one.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class StepOneComponent {
+  private static readonly LOREM_IPSUM_TEXT: string = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+    vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent,
+    luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet.`;
   public readonly formGroup: FormGroup;
 
   constructor(private readonly formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email, Validators.minLength(5)]],
-      downloadsAllowed: [false, Validators.requiredTrue]
-    });
-
-    this.formGroup.valueChanges.subscribe((value: Record<string, string>): void => {
-      const { length: emailLength }: { length: number } = value['email'];
-      const downloadsAllowedControl: AbstractControl<string, string> | null = this.formGroup.get('downloadsAllowed');
-
-      if (emailLength >= 3) {
-        if(downloadsAllowedControl?.disabled) {
-          downloadsAllowedControl?.enable();
-        }
-      } else if(downloadsAllowedControl?.enabled) {
-        downloadsAllowedControl?.disable();
-      }
+      text: [StepOneComponent.LOREM_IPSUM_TEXT, [Validators.required, Validators.nullValidator]],
     });
   }
 }
