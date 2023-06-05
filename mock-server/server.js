2bd9dcb7c50990bd5610d884316cb3a26fdd67dd
Tom S <thoschulte@gmail.com>
2023-06-06 00:30:36 +0200

    update

--- mock-server/server.js
+++ mock-server/server.js
@@ -16,12 +16,14 @@ const app = express().use(cors(), expressWinston.logger({
   msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
   expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
   colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  // ignoreRoute: (req, res) => { return false; } // optional: allows to skip some log messages based on request and/or response
 }));
 
 const storage = multer.diskStorage({
   destination:  (req, file, cb) => {
-    fs.mkdir('./mock-server/videos/',(err)=>{
+    fs.mkdir('./mock-server/videos/',(err)=> {
+      console.error(err);
+
       cb(null, './mock-server/videos/');
     });
   },
